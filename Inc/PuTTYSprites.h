1afe7aec8d49683ccf22d957b179caa1905c8049
CoffeeSum <75074221+CoffeeSum@users.noreply.github.com>
2023-06-16 10:17:04 +0200

    Player movement
    Added player movement alongside minor changes to how sprites get drawn.

--- Inc/PuTTYSprites.h
+++ Inc/PuTTYSprites.h
@@ -4,6 +4,6 @@
 // DEFINES:
 
 void ui_draw_sprite(uint8_t index, uint8_t FGC, uint8_t BGC, uint8_t x, uint8_t y);
-
+void ui_clear_sprite(uint8_t index, uint8_t FGC, uint8_t BGC, uint8_t x, uint8_t y);
 
 #endif /* PUTTYSPRITES_H_ */
